dea53cd44611b63b1788c8134afcf51fe012b656
Silvio Paganini <silvio@fluuu.id>
2019-05-22 16:56:34 +0100

    cleaning up

--- src/components/Awards/Awards.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react'
-import Awards from './Awards';
-storiesOf('Awards', module).add('Awards', () => (
-    <div>
-      <Awards  />
-    </div>
-
-))
